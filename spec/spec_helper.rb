f9c6a5bf578f1a57e033d54d1bd375d347623cca
olezhek85 <olezhek_85@list.ru>
2016-04-12 21:21:30 +0300

    Add SimpleCov to see coverage in CircleCI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,6 @@ require 'codeclimate-test-reporter'
 require 'simplecov'
 
 CodeClimate::TestReporter.start
-
-if ENV['CIRCLE_ARTIFACTS']
-  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
-  SimpleCov.coverage_dir(dir)
-end
-
 SimpleCov.start
 
 RSpec.configure do |config|
